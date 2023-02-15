236dc680416b9ac9c0fb9f8acc07ad7536808eea
sangheonEN <69128174+sangheonEN@users.noreply.github.com>
2023-02-15 15:26:02 +0900

    priority heap and dijkstra example code analysis
    -

--- coding_test/dijkstra_example/network_delay_time.py
+++ coding_test/dijkstra_example/network_delay_time.py
@@ -7,25 +7,49 @@ from collections import deque
 
 class Solution:
     def networkDelayTime(self, times: List[List[int]], N: int, K: int) -> int:
+        """
+
+        :param times: times: [u, v, w] 출발지, 도착지, 소요 시간 입력 list
+        :param N: 전체 노드의 개수
+        :param K: 출발 노드
+        :return: K부터 출발해 모든 노드가 신호를 받을 수 있는 시간 리턴, 불가능할 경우 -1 리턴
+        입력
+        times = [[2,1,1],[2,3,1],[3,4,1]], n = 4, k = 2
+        출력
+        2
+        고찰: 다익스트라는 최단 거리를 구하는 것이기 때문에, 우선순위 heap을 사용한다.
+             우선순위 heap: 새 요소가 힙에 추가되면 자동으로 재구성되어 순서를 유지한다.
+             우선순위 큐를 최소 힙으로 구현하는 경우 가치가 낮은 데이터가 먼저 삭제되고
+             최대 힙으로 구현하는 경우 가치가 높은 데이터가 먼저 삭제된다.
+             그래서 deque를 활용한 queue구조는 node 상태를 update한 뒤에 또 최소, 최대에 따라 정렬을 해주는 작업이 필요하기 때문에 heapq 패키지를 이용한다.
+        """
         graph = collections.defaultdict(list)
 
         for u, v, w in times:
+            # graph[출발노드].append((도착노드, 소요시간))
+            # graph = {출발노드 : [(도착노드, 소요시간)]} 저장됨. -> key는 출발노드, value는 [(도착노드, 소요시간)]
+            # 출발노드가 중복되면 해당 출발노드 key의 value list에 append로 쌓인다.
+            """graph는 전체 node들의 정보를 저장."""
             graph[u].append((v, w))
 
         # K부터 출발해 모든 노드가 신호를 받을 수 있는 시간을 계산
         # Q = [(time, node)] 시작 Node K 부터!
-        Q = deque([(0, K)])
+        # Q의 역할은 출발노드부터 완전 탐색을 시작하면서,
+        # graph에 있는 다음 노드와 소요시간 데이터를 불러와 출발 노드부터 시작한 해당 노드까지의 소요 시간을 업데이트한다.
+        Q = [(0, K)]
+        # Q = deque([(0, K)])
         dist = collections.defaultdict(int)
 
         while Q:
-            # time, node = heapq.heappop(Q)
-            time, node = Q.popleft()
+            time, node = heapq.heappop(Q)
+            # time, node = Q.popleft()
             if node not in dist:
                 dist[node] = time
                 for v, w in graph[node]:
                     alt = time + w
-                    # heapq.heappush(Q, (alt, v))
-                    Q.append((alt, v))
+                    heapq.heappush(Q, (alt, v))
+                    # Q.append((alt, v))
+
         if len(dist) == N:
             return max(dist.values())
 
---
+++ coding_test/priority_queue/basic.py
@@ -0,0 +1,76 @@
+#%%
+
+"""
+일반적으로 최단, 최소를 구할때 사용함.
+특히, 다익스트라는 최단, 최대 거리를 구하는 문제에 사용함.
+      1. 우선순위 heap: 새 요소가 힙에 추가되면 자동으로 재구성되어 순서를 유지한다.
+      2. 우선순위 큐를 최소 힙으로 구현하는 경우 가치가 낮은 데이터가 먼저 삭제되고
+      3. 최대 힙으로 구현하는 경우 가치가 높은 데이터가 먼저 삭제된다.
+      4. 그래서 deque를 활용한 queue구조는 node 상태를 update한 뒤에 또 최소, 최대에 따라 정렬을 해주는 작업이 필요하기 때문에 heapq 패키지를 이용한다.
+"""
+import heapq
+
+# heappush를 할때마다, 새 요소가 힙에 추가되면서 자동으로 최소 힙으로 재구성되어 순서를 유지한다.
+# 1. 최소 우선 순위 힙 구조
+Q = []
+heapq.heappush(Q, 5)
+heapq.heappush(Q, 4)
+heapq.heappush(Q, 10)
+heapq.heappush(Q, 2)
+print(Q)
+
+# 2. 최대 우선 순회 힙 구조
+num = [5, 2, 3, 2, 10, 1]
+heap = []
+max_heap = []
+
+# 0 index에 n의 음수 값을 저장해서, 가장 낮은 수가 큰 값으로 우선 순위되도록 힙 정렬을 함.
+for n in num:
+    heapq.heappush(heap, (-n, n))
+
+print(heapq)
+
+while heap:
+    max_heap.append(heapq.heappop(heap)[-1])
+
+print(max_heap)
+
+# 3. 이미 원소가 있는 list를 heap으로 바꾸기
+Q2 = [4, 2, 1, 2, 10]
+heapq.heapify(Q2)
+
+print(Q2)
+
+# 4. n번째 최소값 or 최대값 출력
+Q4 = [4, 2, 1, 2, 10]
+n = 2
+def nth_smallest(arr, n):
+    heap = []
+    for num in arr:
+        # 최소 우선 순위 힙으로 정렬됨.
+        heapq.heappush(heap, num)
+
+    nth_min = None
+
+    for _ in range(n):
+        # stack 구조로 최소 값이 저장된 첫순서부터 n번 순서까지 pop.
+        nth_min = heapq.heappop(heap)
+
+    return nth_min
+
+def nth_biggest(arr, n):
+    heap = []
+    for num in arr:
+        heapq.heappush(heap, (-num, num))
+
+    nth_max = None
+
+    for _ in range(n):
+        nth_max = heapq.heappop(heap)[-1]
+
+    return nth_max
+
+print(nth_smallest(Q4, n))
+print(nth_biggest(Q4, n))
+
+
